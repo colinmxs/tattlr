4d170a696f09372f4a92de37938912dcdd6fe9f4
philbot5000 <philbot5000@gmail.com>
2014-06-26 19:24:51 -0600

    First

---
+++ tattlr.ui/app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+  .module('tattlrApp', [
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
