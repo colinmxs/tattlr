1818afcf576ba0663db19585dd742a43184d745f
philbot5000 <philbot5000@gmail.com>
2014-06-26 19:56:45 -0600

    Route Test for Build

--- tattlr.ui/app/scripts/app.js
+++ tattlr.ui/app/scripts/app.js
@@ -6,13 +6,19 @@ angular
     'ngSanitize',
     'ngRoute'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
+
+    $locationProvider.html5Mode(true);
   });
