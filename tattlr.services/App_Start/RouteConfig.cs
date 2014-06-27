1f09cd899bdc4a9d229b064ff1916bc9f5a89835
colinmxs <smith.colin00@gmail.com>
2014-06-26 18:55:09 -0600

    routing stuff

--- tattlr.services/App_Start/RouteConfig.cs
+++ tattlr.services/App_Start/RouteConfig.cs
@@ -8,6 +8,7 @@ namespace tattlr.services
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("");
 
             routes.MapRoute(
                 name: "Default",
