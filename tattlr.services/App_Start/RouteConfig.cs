bd1edbb74667a0b94aba0a159a9b1523002188f7
colinmxs <smith.colin00@gmail.com>
2014-06-30 17:40:05 -0600

    kl;jasd

--- tattlr.services/App_Start/RouteConfig.cs
+++ tattlr.services/App_Start/RouteConfig.cs
@@ -8,7 +8,13 @@ namespace tattlr.services
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            routes.IgnoreRoute("");
+            //routes.IgnoreRoute("");
+
+            routes.MapRoute(
+                "Default",                                              // Route name
+                "{controller}/{action}/{id}",                           // URL with parameters
+                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
+            );
         }
     }
 }
